34eb91599b41cdf4bb2490b7b8e88ed197754582
Ralph Ursprung <ralph.ursprung@gmail.com>
2022-11-12 20:34:11 +0100

    use interrupt

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ mod app {
     use stm32f4xx_hal::pac::IWDG;
     use stm32f4xx_hal::rcc::{Clocks, Rcc};
     use stm32f4xx_hal::{
-        gpio::{PB8, PB9},
+        gpio::{Edge, PB8, PB9},
         i2c::{I2c, I2c1},
         pac,
         prelude::*,
@@ -43,7 +43,9 @@ mod app {
     }
 
     #[init]
-    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+    fn init(mut ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        let mut syscfg = ctx.device.SYSCFG.constrain();
+
         let rcc = ctx.device.RCC.constrain();
         let clocks = setup_clocks(rcc);
         let mono = ctx.device.TIM2.monotonic_us(&clocks);
@@ -56,12 +58,18 @@ mod app {
         let gpiob = ctx.device.GPIOB.split();
         let i2c = I2c::new(ctx.device.I2C1, (gpiob.pb8, gpiob.pb9), 400.kHz(), &clocks);
 
+        let gpioa = ctx.device.GPIOA.split();
+        let mut tof_data_interrupt = gpioa.pa0.into_pull_down_input();
+        tof_data_interrupt.make_interrupt_source(&mut syscfg);
+        tof_data_interrupt.enable_interrupt(&mut ctx.device.EXTI);
+        tof_data_interrupt.trigger_on_edge(&mut ctx.device.EXTI, Edge::Rising);
+
         // set up the TOF sensor
         let tof_sensor = setup_tof(i2c, &mut delay);
 
         defmt::info!("init done!");
 
-        poll_tof::spawn().ok();
+        //poll_tof::spawn().ok();
 
         (
             Shared { delay },
@@ -81,7 +89,7 @@ mod app {
     /// Set up the independent watchdog and start the period task to feed it
     fn setup_watchdog(iwdg: IWDG) -> IndependentWatchdog {
         let mut watchdog = IndependentWatchdog::new(iwdg);
-        watchdog.start(500u32.millis());
+        watchdog.start(1000u32.millis());
         watchdog.feed();
         periodic::spawn().ok();
         defmt::trace!("watchdog set up");
@@ -94,39 +102,15 @@ mod app {
         D: vl53l1::Delay,
     {
         let mut vl53l1_dev = vl53l1::Device::default();
-        defmt::info!("Software reset...");
         vl53l1::software_reset(&mut vl53l1_dev, &mut i2c, delay).unwrap();
-        defmt::info!("  Complete");
-
-        defmt::info!("Data init...");
         vl53l1::data_init(&mut vl53l1_dev, &mut i2c).unwrap();
-        defmt::info!("  Complete");
-
-        defmt::info!("Static init...");
         vl53l1::static_init(&mut vl53l1_dev).unwrap();
-        defmt::info!("  Complete");
 
-        defmt::info!("Setting region of interest...");
-        let roi = vl53l1::UserRoi {
-            bot_right_x: 10,
-            bot_right_y: 6,
-            top_left_x: 6,
-            top_left_y: 10,
-        };
-        vl53l1::set_user_roi(&mut vl53l1_dev, roi).unwrap();
-        defmt::info!("  Complete");
-
-        defmt::info!("Setting timing budget and inter-measurement period...");
+        // TODO: find numbers which work well for this project (don't trigger too often but do trigger often enough)
         vl53l1::set_measurement_timing_budget_micro_seconds(&mut vl53l1_dev, 50_000).unwrap();
         vl53l1::set_inter_measurement_period_milli_seconds(&mut vl53l1_dev, 60).unwrap();
 
-        defmt::info!("Start measurement...");
         vl53l1::start_measurement(&mut vl53l1_dev, &mut i2c).unwrap();
-        defmt::info!("  Complete");
-
-        defmt::info!("Wait measurement data ready...");
-        vl53l1::wait_measurement_data_ready(&mut vl53l1_dev, &mut i2c, delay).unwrap();
-        defmt::info!("  Ready");
 
         TOFSensor {
             device: vl53l1_dev,
@@ -134,32 +118,32 @@ mod app {
         }
     }
 
-    //#[task(binds=EXTI0, shared=[delay], local=[tof_sensor])]
-    //fn tof_interrupt_triggered(mut ctx: tof_interrupt_triggered::Context) {
-    #[task(priority=1, shared=[delay], local=[tof_sensor])]
-    fn poll_tof(mut ctx: poll_tof::Context) {
+    /// Triggers every time the TOF has data (= new range measurement) available to be consumed.
+    #[task(binds=EXTI0, shared=[delay], local=[tof_sensor])]
+    fn tof_interrupt_triggered(mut ctx: tof_interrupt_triggered::Context) {
         let vl53l1_dev = &mut ctx.local.tof_sensor.device;
         let i2c = &mut ctx.local.tof_sensor.i2c;
         ctx.shared.delay.lock(|delay| {
-            defmt::info!("Wait measurement data ready...");
-            vl53l1::wait_measurement_data_ready(vl53l1_dev, i2c, delay).unwrap();
-            defmt::info!("  Ready");
-
-            defmt::info!("Get ranging measurement data...");
             let rmd = vl53l1::get_ranging_measurement_data(vl53l1_dev, i2c).unwrap();
             vl53l1::clear_interrupt_and_start_measurement(vl53l1_dev, i2c, delay).unwrap();
-            defmt::info!("  {:#?} mm", rmd.range_milli_meter);
+            match rmd.range_status {
+                vl53l1::RangeStatus::RANGE_VALID => {
+                    defmt::info!("Received range: {}mm", rmd.range_milli_meter)
+                }
+                status => defmt::warn!(
+                    "Received invalid range status: {:?}",
+                    defmt::Debug2Format(&status)
+                ),
+            }
         });
-
-        poll_tof::spawn_after(1.secs()).ok();
     }
 
     /// Feed the watchdog to avoid hardware reset.
-    #[task(priority=5, local=[watchdog])]
+    #[task(priority=1, local=[watchdog])]
     fn periodic(ctx: periodic::Context) {
         defmt::trace!("feeding the watchdog!");
         ctx.local.watchdog.feed();
 
-        periodic::spawn_after(100.millis()).ok();
+        periodic::spawn_after(200.millis()).ok();
     }
 }
